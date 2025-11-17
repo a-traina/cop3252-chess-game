35f179d45085641f9474e832ec2281f9170ddb5e
Alyssa Traina <lyssatraina@gmail.com>
2025-11-16 23:00:34 -0500

    Define Player class and add Player initialization logic

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,3 +1,16 @@
 public class GameBoard {
-    
+    private Player player1;
+    private Player player2;
+    private Piece[][] board = new Piece[8][8];
+
+    public GameBoard() {
+        player1 = new Player("white");
+        player2 = new Player("black");
+
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 8; j++) {
+                board[i][j] = null;
+            }
+        }
+    }
 }
