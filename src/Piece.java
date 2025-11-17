18e7f3dfb0cb6b44d56f851e8126c8ea555fe3b1
Alyssa Traina <lyssatraina@gmail.com>
2025-11-16 22:20:43 -0500

    Create Piece, GameBoard, and Position class

--- src/Piece.java
+++ src/Piece.java
@@ -1,3 +1,7 @@
+import java.util.HashSet;
+
 public abstract class Piece {
     protected int pointValue;
+    protected String color;
+    abstract HashSet<Position> getAllMoves(Position p, GameBoard gb);
 }
---
+++ src/Position.java
@@ -0,0 +1,37 @@
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) {
+            return true;
+        }
+
+        else if(!(o instanceof Position)) {
+            return false;
+        }
+
+        Position p = (Position) o;
+
+        return this.x == p.x && this.y == p.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return x + 13 * y;
+    }
+}
