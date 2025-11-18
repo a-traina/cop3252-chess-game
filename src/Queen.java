75beafa85f13bf4861fb52ebd59bc44a2ce9066e
Alyssa Traina <lyssatraina@gmail.com>
2025-11-17 19:59:07 -0500

    Implement Queen piece move logic

--- src/Queen.java
+++ src/Queen.java
@@ -8,8 +8,123 @@ public class Queen extends Piece {
 
     @Override
     public HashSet<Position> getAllMoves(Position p, GameBoard gb) {
-        HashSet<Position> set = new HashSet<>();
+        HashSet<Position> moveSet = new HashSet<>();
 
-        return set;
+        int x = p.getX();
+        int y = p.getY();
+
+        for(int i = x + 1; i < 8; i++) {
+            Piece pieceAt = gb.getPieceAt(i, y);
+            if(pieceAt == null) {
+                moveSet.add(new Position(i, y));
+            }
+            else if(!pieceAt.color.equals(this.color)) {
+                moveSet.add(new Position(i, y));
+                break;
+            }
+            else {
+                break;
+            }
+        }
+
+        for(int i = x - 1; i >= 0; i--) {
+            Piece pieceAt = gb.getPieceAt(i, y);
+            if(pieceAt == null) {
+                moveSet.add(new Position(i, y));
+            }
+            else if(!pieceAt.color.equals(this.color)) {
+                moveSet.add(new Position(i, y));
+                break;
+            }
+            else {
+                break;
+            }
+        }
+
+        for(int j = y - 1; j >= 0; j--) {
+            Piece pieceAt = gb.getPieceAt(x, j);
+            if(pieceAt == null) {
+                moveSet.add(new Position(x, j));
+            }
+            else if(!pieceAt.color.equals(this.color)) {
+                moveSet.add(new Position(x, j));
+                break;
+            }
+            else {
+                break;
+            }
+        }
+
+        for(int j = y + 1; j < 8; j++) {
+            Piece pieceAt = gb.getPieceAt(x, j);
+            if(pieceAt == null) {
+                moveSet.add(new Position(x, j));
+            }
+            else if(!pieceAt.color.equals(this.color)) {
+                moveSet.add(new Position(x, j));
+                break;
+            }
+            else {
+                break;
+            }
+        }
+
+        for(int i = x + 1, j = y + 1; i < 8 && j < 8; i++, j++) {
+            Piece pieceAt = gb.getPieceAt(i, j);
+            if(pieceAt == null) {
+                moveSet.add(new Position(i, j));
+            }
+            else if(!pieceAt.color.equals(this.color)) {
+                moveSet.add(new Position(i, j));
+                break;
+            }
+            else {
+                break;
+            }
+        }
+
+        for(int i = x - 1, j = y + 1; i >=0 && j < 8; i--, j++) {
+            Piece pieceAt = gb.getPieceAt(i, j);
+            if(pieceAt == null) {
+                moveSet.add(new Position(i, j));
+            }
+            else if(!pieceAt.color.equals(this.color)) {
+                moveSet.add(new Position(i, j));
+                break;
+            }
+            else {
+                break;
+            }
+        }
+
+        for(int i = x - 1, j = y - 1; i >=0 && j >= 0; i--, j--) {
+            Piece pieceAt = gb.getPieceAt(i, j);
+            if(pieceAt == null) {
+                moveSet.add(new Position(i, j));
+            }
+            else if(!pieceAt.color.equals(this.color)) {
+                moveSet.add(new Position(i, j));
+                break;
+            }
+            else {
+                break;
+            }
+        }
+
+        for(int i = x + 1, j = y - 1; i < 8 && j >= 0; i++, j--) {
+            Piece pieceAt = gb.getPieceAt(i, j);
+            if(pieceAt == null) {
+                moveSet.add(new Position(i, j));
+            }
+            else if(!pieceAt.color.equals(this.color)) {
+                moveSet.add(new Position(i, j));
+                break;
+            }
+            else {
+                break;
+            }
+        }
+
+        return moveSet;
     }
 }
