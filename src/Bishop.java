ec43ac96a002a6cb088d11586578979f7c6047ad
ethan-broome <etbroome05@gmail.com>
2025-11-17 19:54:02 -0500

    Bishop Move Implementation

---
+++ src/Bishop.java
@@ -0,0 +1,86 @@
+import java.util.HashSet;
+
+public class Bishop extends Piece {
+
+    public Bishop(String color) {
+        pointValue = 3;
+        this.color = color;
+    }
+
+    HashSet<Position> getAllMoves(Position p, GameBoard gb) {
+
+        //local variables
+        int x = p.getX();
+        int y = p.getY();
+
+        //output set
+        HashSet<Position> moveSet = new HashSet<Position>();
+
+        //check for NE moves
+        for (int i = x + 1, j = y + 1; i < 8 && j < 8; i++, j++) {
+            Piece pieceAt = gb.getPieceAt(i, j);
+
+            if (pieceAt == null) {
+                moveSet.add(new Position(i, j));
+                continue;
+            }
+            else if (pieceAt.color.equals(this.color)) {
+                moveSet.add(new Position(i, j));
+                break;
+            }
+            else
+                break;
+        }
+
+        //check for NW moves
+        for (int i = x + 1, j = y + 1; i < 8 && j < 8; i++, j--) {
+            Piece pieceAt = gb.getPieceAt(i, j);
+
+            if (pieceAt == null) {
+                moveSet.add(new Position(i, j));
+                continue;
+            }
+            else if (pieceAt.color.equals(this.color)) {
+                moveSet.add(new Position(i, j));
+                break;
+            }
+            else
+                break;
+        }
+
+        //check for SE moves
+        for (int i = x + 1, j = y + 1; i < 8 && j < 8; i--, j++) {
+            Piece pieceAt = gb.getPieceAt(i, j);
+
+            if (pieceAt == null) {
+                moveSet.add(new Position(i, j));
+                continue;
+            }
+            else if (pieceAt.color.equals(this.color)) {
+                moveSet.add(new Position(i, j));
+                break;
+            }
+            else
+                break;
+        }
+
+        //check for SW moves
+        for (int i = x + 1, j = y + 1; i < 8 && j < 8; i--, j--) {
+            Piece pieceAt = gb.getPieceAt(i, j);
+
+            if (pieceAt == null) {
+                moveSet.add(new Position(i, j));
+                continue;
+            }
+            else if (pieceAt.color.equals(this.color)) {
+                moveSet.add(new Position(i, j));
+                break;
+            }
+            else
+                break;
+        }
+
+        return moveSet;
+    }
+
+}
