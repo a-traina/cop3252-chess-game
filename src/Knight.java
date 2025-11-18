2011f02fbc20110a6f7674a880da064a94928f61
ethan-broome <etbroome05@gmail.com>
2025-11-17 22:56:39 -0500

    Created Knight Class and Implemented move logic

--- src/Knight.java
+++ src/Knight.java
@@ -2,6 +2,18 @@ import java.util.HashSet;
 
 public class Knight extends Piece {
 
+    private boolean realSquare(int v1, int v2) {
+        if (v1 < 0)
+            return false;
+        if (v1 > 7)
+            return false;
+        if (v2 < 0)
+            return false;
+        if (v2 > 7)
+            return false;
+        return true;
+    }
+
     Knight(String color) {
         this.color = color;
         pointValue = 3;
@@ -16,8 +28,39 @@ public class Knight extends Piece {
         //output set
         HashSet<Position> moveSet = new HashSet<Position>();
 
-        if ()
+        //move logic
+        if (realSquare(x - 1, y - 2) && !gb.getPieceAt(x - 1, y - 2).color.equals(this.color)) {
+            moveSet.add(new Position(x - 1, y - 2));
+        }
 
+        if (realSquare(x - 2, y - 1) && !gb.getPieceAt(x - 2, y - 1).color.equals(this.color)) {
+            moveSet.add(new Position(x - 2, y - 1));
+        }
 
+        if (realSquare(x - 2, y + 1) && !gb.getPieceAt(x - 2, y + 1).color.equals(this.color)) {
+            moveSet.add(new Position(x - 2, y + 1));
+        }
+
+        if (realSquare(x - 1, y + 2) && !gb.getPieceAt(x - 1, y + 2).color.equals(this.color)) {
+            moveSet.add(new Position(x - 1, y + 2));
+        }
+
+        if (realSquare(x + 1, y - 2) && !gb.getPieceAt(x + 1, y - 2).color.equals(this.color)) {
+            moveSet.add(new Position(x + 1, y - 2));
+        }
+
+        if (realSquare(x + 2, y - 1) && !gb.getPieceAt(x + 2, y - 1).color.equals(this.color)) {
+            moveSet.add(new Position(x + 2, y - 1));
+        }
+
+        if (realSquare(x + 2, y + 1) && !gb.getPieceAt(x + 2, y + 1).color.equals(this.color)) {
+            moveSet.add(new Position(x + 2, y + 1));
+        }
+
+        if (realSquare(x + 1, y + 2) && !gb.getPieceAt(x + 1, y + 2).color.equals(this.color)) {
+            moveSet.add(new Position(x + 1, y + 2));
+        }
+
+        return moveSet;
     }
 }
