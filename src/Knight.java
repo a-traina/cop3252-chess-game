7ed5778e8688619236eabf580af57e1fd19c3b8f
ethan-broome <etbroome05@gmail.com>
2025-11-17 22:19:21 -0500

    Added Knight class

---
+++ src/Knight.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class Knight extends Piece {
+
+    Knight(String color) {
+        this.color = color;
+        pointValue = 3;
+    }
+
+    HashSet<Position> getAllMoves(Piece p, GameBoard gb) {
+
+    }
+}
