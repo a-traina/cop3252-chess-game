a86ff52b89b0da3d86792ce061df05f49b235ced
ethan-broome <etbroome05@gmail.com>
2025-11-17 23:00:24 -0500

    Fixed logic for null squares

--- src/Knight.java
+++ src/Knight.java
@@ -29,35 +29,35 @@ public class Knight extends Piece {
         HashSet<Position> moveSet = new HashSet<Position>();
 
         //move logic
-        if (realSquare(x - 1, y - 2) && !gb.getPieceAt(x - 1, y - 2).color.equals(this.color)) {
+        if (realSquare(x - 1, y - 2) && (!gb.getPieceAt(x - 1, y - 2).color.equals(this.color) || gb.getPieceAt(x - 1, y - 2) == null)) {
             moveSet.add(new Position(x - 1, y - 2));
         }
 
-        if (realSquare(x - 2, y - 1) && !gb.getPieceAt(x - 2, y - 1).color.equals(this.color)) {
+        if (realSquare(x - 2, y - 1) && (!gb.getPieceAt(x - 2, y - 1).color.equals(this.color) || gb.getPieceAt(x - 2, y - 1) == null)) {
             moveSet.add(new Position(x - 2, y - 1));
         }
 
-        if (realSquare(x - 2, y + 1) && !gb.getPieceAt(x - 2, y + 1).color.equals(this.color)) {
+        if (realSquare(x - 2, y + 1) && (!gb.getPieceAt(x - 2, y + 1).color.equals(this.color) || gb.getPieceAt(x - 2, y + 1) == null)) {
             moveSet.add(new Position(x - 2, y + 1));
         }
 
-        if (realSquare(x - 1, y + 2) && !gb.getPieceAt(x - 1, y + 2).color.equals(this.color)) {
+        if (realSquare(x - 1, y + 2) && (!gb.getPieceAt(x - 1, y + 2).color.equals(this.color) || gb.getPieceAt(x - 1, y + 2) == null)) {
             moveSet.add(new Position(x - 1, y + 2));
         }
 
-        if (realSquare(x + 1, y - 2) && !gb.getPieceAt(x + 1, y - 2).color.equals(this.color)) {
+        if (realSquare(x + 1, y - 2) && (!gb.getPieceAt(x + 1, y - 2).color.equals(this.color) || gb.getPieceAt(x + 1, y - 2) == null)) {
             moveSet.add(new Position(x + 1, y - 2));
         }
 
-        if (realSquare(x + 2, y - 1) && !gb.getPieceAt(x + 2, y - 1).color.equals(this.color)) {
+        if (realSquare(x + 2, y - 1) && (!gb.getPieceAt(x + 2, y - 1).color.equals(this.color) || gb.getPieceAt(x + 2, y - 1) == null)) {
             moveSet.add(new Position(x + 2, y - 1));
         }
 
-        if (realSquare(x + 2, y + 1) && !gb.getPieceAt(x + 2, y + 1).color.equals(this.color)) {
+        if (realSquare(x + 2, y + 1) && (!gb.getPieceAt(x + 2, y + 1).color.equals(this.color) || gb.getPieceAt(x + 2, y + 1) == null)) {
             moveSet.add(new Position(x + 2, y + 1));
         }
 
-        if (realSquare(x + 1, y + 2) && !gb.getPieceAt(x + 1, y + 2).color.equals(this.color)) {
+        if (realSquare(x + 1, y + 2) && (!gb.getPieceAt(x + 1, y + 2).color.equals(this.color) || gb.getPieceAt(x + 1, y + 2) == null)) {
             moveSet.add(new Position(x + 1, y + 2));
         }
 
