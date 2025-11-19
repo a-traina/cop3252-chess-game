5125ef151da4b4a354b586abc2c3f624407fdf48
Alyssa Traina <lyssatraina@gmail.com>
2025-11-19 13:16:38 -0500

    Add piece capturing logic to make move method

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -35,26 +35,38 @@ public class GameBoard {
         return new Position(0, 0);
     }
 
-    public void makeMove(Piece piece, Position newPos) {
-        if (piece == null) return;
+    public boolean makeMove(Piece piece, Position newPos) {
+        if (piece == null || !piece.getColor().equals(currTurn)) return false;
 
         for(int i = 0; i < 8; i++) {
             for(int j = 0; j < 8; j++) {
                 if(board[i][j] == piece) {
-                    if(newPos.equals(new Position(i, j))) return;
+                    if(newPos.equals(new Position(i, j))) return false;
 
                     HashSet<Position> moveSet = piece.getAllMoves(new Position(i, j), this);
                     if(moveSet.contains(newPos)) {
+                        if(board[newPos.getX()][newPos.getY()] != null) {
+                            if(currTurn.equals("white")) {
+                                player1.capturePiece(board[newPos.getX()][newPos.getY()]);
+                                player2.deletePiece(board[newPos.getX()][newPos.getY()]);
+                            }
+                            else {
+                                player2.capturePiece(board[newPos.getX()][newPos.getY()]);
+                                player1.deletePiece(board[newPos.getX()][newPos.getY()]);
+                            }
+                        }
+
                         board[newPos.getX()][newPos.getY()] = piece;
                         board[i][j] = null;
                         break;
                     }
-                    else return;
+                    else return false;
                 }
             }
         }
         
         // Change current player turn
         currTurn = currTurn.equals("white") ? "black" : "white";
+        return true;
     }
 }
