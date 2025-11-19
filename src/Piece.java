d22da3bb7e9f28721e585d2094998d64feb920d1
Alyssa Traina <lyssatraina@gmail.com>
2025-11-19 12:42:53 -0500

    Add method to retrieve color for all pieces

--- src/Piece.java
+++ src/Piece.java
@@ -4,4 +4,7 @@ public abstract class Piece {
     protected int pointValue;
     protected String color;
     abstract public HashSet<Position> getAllMoves(Position p, GameBoard gb);
+    public String getColor() {
+        return color;
+    }
 }
