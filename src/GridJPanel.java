833588162c3d093f7ba326acaa304f5ec27e1bd8
Alyssa Traina <lyssatraina@gmail.com>
2025-12-03 03:05:24 -0500

    After move is made, resets to default cursor for clean player switching behavior

--- src/GridJPanel.java
+++ src/GridJPanel.java
@@ -122,6 +122,7 @@ public class GridJPanel extends JPanel {
                             Position target = new Position(row, col);
                             if (highlightedMoves.contains(target)) {
                                 if (gameBoard.makeMove(selectedPosition, target)) {
+                                    setCursor(Cursor.getDefaultCursor());
                                     if (gameBoard.getTurn().getColor().equals("white")) {
                                         String move = gameBoard.getMoveHistory().get(gameBoard.getMoveNumber());
                                         String[] row = move.split(" ");
